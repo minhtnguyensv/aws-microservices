cc0f46646cbc566ae845a839e7b2ac7fca33fcd1
Minh T Nguyen <minh.nguyen@service.vic.gov.au>
2022-09-26 16:41:53 +1000

    add basket table

--- lib/database.ts
+++ lib/database.ts
@@ -10,10 +10,13 @@ import { RemovalPolicy } from 'aws-cdk-lib';
 // Swn - just random name
 export class SwnDatabase extends Construct {
   public readonly productTable: ITable;
+  public readonly basketTable: ITable;
 
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
+    // Product DynamoDb table creation
+    // product: PK: id -- name -- description -- imageFile -- price -- category
     const productTable = new Table(this, 'product', {
       partitionKey: {
         name: 'id',
@@ -25,5 +28,22 @@ export class SwnDatabase extends Construct {
     });
 
     this.productTable = productTable;
+
+    // Basket table
+    // Basket PK-userName -- items (SET-MAP object)
+    // item 1 - { quantity - color - price - productId - productName }
+    // item 2 - { quantity - color - price - productId - productName }
+
+    const basketTable = new Table(this, 'basket', {
+      partitionKey: {
+        name: 'userName',
+        type: AttributeType.STRING,
+      },
+      tableName: 'basket',
+      removalPolicy: RemovalPolicy.DESTROY,
+      billingMode: BillingMode.PAY_PER_REQUEST,
+    });
+
+    this.basketTable = basketTable;
   }
 }
